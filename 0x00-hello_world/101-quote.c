0c8e034033d91c6fa1e21b24d2eb5622bffa965c
danimohlife <danimohlife@yahoo.com>
2023-05-18 14:20:49 +0100

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main 4 all
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+	char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	while (x < 59)
+	{
+		putchar(str[x]);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
