a4eb616c170d1a5231055da2c4dc4721e1c507f0
danimohlife <danimohlife@yahoo.com>
2023-05-18 14:35:20 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - main 4 all
  * Return: 0
  */
 int main(void)
 {
-	int x = 0;
-	char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	while (x < 59)
-	{
-		putchar(str[x]);
-		x++;
-	}
-	return (0);
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	return (1);
 }
