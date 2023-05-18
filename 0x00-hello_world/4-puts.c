9899f8afe4e42f223c8aca104cb9ceff6191612f
danimohlife <danimohlife@yahoo.com>
2023-05-18 10:36:57 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main 4/as root func
+ * Return: 0 as success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
