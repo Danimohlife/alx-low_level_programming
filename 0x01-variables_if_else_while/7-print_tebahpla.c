a9e79fe9bb7a29b74b91e74d43ae0322ae8fb082
danimohlife <danimohlife@yahoo.com>
2023-05-19 16:57:05 +0100

    mirrow the elimswq

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - confirmed
+ * Return: 0 accepted
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
