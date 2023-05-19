74b3975acc0870526cad4a857105131d87c4ade3
danimohlife <danimohlife@yahoo.com>
2023-05-19 16:35:21 +0100

    missed mr 'e' & mr 'q'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - 4 main
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'e' || x == 'q')
+			continue;
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
