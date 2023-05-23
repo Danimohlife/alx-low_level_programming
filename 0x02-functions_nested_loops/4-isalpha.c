b49cccbef86b1fd40569001948c657dc6e579e43
danimohlife <danimohlife@yahoo.com>
2023-05-23 11:20:01 +0100

    i b alpha or lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - funct alpha
+ * Return: 1 / 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
