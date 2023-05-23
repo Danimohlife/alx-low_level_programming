c62967d0d75edafcfc6360ab4665728992b30822
danimohlife <danimohlife@yahoo.com>
2023-05-23 14:23:07 +0100

    I hate- nums

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _abs - I no like low numbers
- * return: int i like
+ * @c: argu
+ * Return: int i like
  */
 int _abs(int c)
 {
 	if (c < 0)
-		return ( c * (-1));
+		return (c * (-1));
 	return (c);
 }
