dd8da1ed1c01deddbed81d7bf57705b67add41b7
danimohlife <danimohlife@yahoo.com>
2023-05-24 10:50:55 +0100

    at ur back, pls

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,9 +15,9 @@ int print_last_digit(int n)
 
 	if (n >= 0 && n <= 9)
 	{
-		s = n;
-		_putchar((s / 10) + '0');
-		_putchar((s % 10) + '0');
+		n = n / 10;
+		s = n % 10;
+		_putchar(s + '0');
 	}
 	else if (n > 9)
 	{
