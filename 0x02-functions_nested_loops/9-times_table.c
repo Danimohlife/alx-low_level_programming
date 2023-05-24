ed60f285cb43b2d0f23f5fa18672e6658372c552
danimohlife <danimohlife@yahoo.com>
2023-05-24 13:29:02 +0100

    math red

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,27 +6,22 @@
  */
 void times_table(void)
 {
-	int x, y;
+	int x, y, z;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			if (x * y <= 9)
-				_putchar((x * y % 10) + '0');
-			else if (x * y > 9)
-			{
-				_putchar((x * y / 10) + '0');
-				_putchar((x * y % 10) + '0');
-			}
-			if (y < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (y == 9)
-				_putchar('\n');
+			z = x * y;
 
+			if (z <= 9 && y < 9)
+				printf("%d,  ", z);
+			else if (z <= 9 && y == 9)
+				printf("%d\n", z);
+			else if (z > 9 && y < 9)
+				printf("%d, ", z);
+			else if (z >= 10 && y == 9)
+				printf("%d\n", z);
 		}
 	}
 }
