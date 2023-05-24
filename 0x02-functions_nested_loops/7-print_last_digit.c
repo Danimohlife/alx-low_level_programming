8e823e31fc126367869549287c42b76bf9ccb9e6
danimohlife <danimohlife@yahoo.com>
2023-05-24 21:31:45 +0100

    at ur back, pls

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <limits.h>
+#include <stdio.h>
 /**
  * print_last_digit - I dey your back please
  * @n: aurg
@@ -9,10 +10,10 @@ int print_last_digit(int n)
 	int s;
 
 	if (n < 0)
-	{
 		n = -n;
-	}
 	s = n % 10;
+	if (s < 0)
+		s = -s;
 	_putchar(s + '0');
 	return (s);
 }
