e55aab8e5181046da00dcb754e5353149118e164
danimohlife <danimohlife@yahoo.com>
2023-05-25 12:12:24 +0100

    up is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - upper case
+ * @c: cheack against c
+ * Return: 1 / 0
+ */
+int _isupper(int c)
+{
+	if (c >= 41 && c <= 60)
+		return (1);
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Going further on the function and nested loops. But this time self valuable function are created.
