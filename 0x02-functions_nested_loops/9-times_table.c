3e907f659cd86970ffc834b094bfc4abd5e72917
danimohlife <danimohlife@yahoo.com>
2023-05-27 22:18:45 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,14 +14,36 @@ void times_table(void)
 		{
 			z = x * y;
 
-			if (z <= 9 && y < 9)
-				printf("%d,  ", z);
-			else if (z <= 9 && y == 9)
-				printf("%d\n", z);
-			else if (z > 9 && y < 9)
-				printf("%d, ", z);
-			else if (z >= 10 && y == 9)
-				printf("%d\n", z);
+			if (z != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (z >= 10)
+			{
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else if (z < 10 && y != 0)
+			{
+				_putchar(' ');
+				_putchar((z % 10) + '0');
+				if (x == 0 && y < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((z % 10) + '0');
+				if (x == 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
+		_putchar('\n');
 	}
 }
