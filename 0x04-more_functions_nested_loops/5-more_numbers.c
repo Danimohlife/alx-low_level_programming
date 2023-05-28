9bd8244ea6f7a7dea6c508f2a4f9aa93895cbe07
danimohlife <danimohlife@yahoo.com>
2023-05-28 22:29:36 +0100

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - number
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
