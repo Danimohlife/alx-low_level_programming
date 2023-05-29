7e8795f02d02c1ac5197c70a2075400176b59c20
danimohlife <danimohlife@yahoo.com>
2023-05-29 17:46:19 +0100

    Sorry I write in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - reverse writting
+ * @s: str in rev pointer
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int j, i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (j = i; j >= 0; j--)
+		_putchar(s[j]);
+	_putchar('\n');
+}
