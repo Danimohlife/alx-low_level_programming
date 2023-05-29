7075e4628ae00abe86066bbdc07884a171ad13b2
danimohlife <danimohlife@yahoo.com>
2023-05-29 17:17:42 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - lenght of string
+ * @s: pointer to str
+ * Return: len of s
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
