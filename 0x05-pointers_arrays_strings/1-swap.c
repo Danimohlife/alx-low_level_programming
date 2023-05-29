cc51bc3b2ac5254e8123f089477b8c935014f015
danimohlife <danimohlife@yahoo.com>
2023-05-29 17:04:41 +0100

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - exchange item using pointer
+ * @i: first item
+ * @j: second item
+ * Return: nothing
+ */
+void swap_int(int *i, int *j)
+{
+	int k;
+	k = *i;
+	*i = *j;
+	*j = k;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 void reset_to_98(int *);
+void swap_int(int *, int *);
+
 #endif
