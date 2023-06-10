4a51da87a941ad5333ebe816de4b58aab367c138
danimohlife <danimohlife@yahoo.com>
2023-06-10 01:07:04 +0100

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main to run other functs
+ * @argc: the counting for ....
+ * @argv: the vector
+ * Return: 0 on successfull
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Commandline aurgument is used to pass information into the programme after compilation. Just like passing inform or data into function calls but this time the function is main().
