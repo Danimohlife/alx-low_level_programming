77c178bf2a184708ece824b49079403b942098b1
danimohlife <danimohlife@yahoo.com>
2023-06-14 22:02:19 +0100

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main for change
+ * @argc: first arg
+ * @argv: second char argu
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int  i, change = 0;
+
+	i = atoi(argv[1]);
+	if (args != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (i < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i >= 0)
+	{
+		if (i >= 25)
+			i -= 25;
+
+		else if (i >= 10)
+			i -= 10;
+
+		else if (i >= 5)
+			i -= 5;
+
+		else if (i >= 2)
+			i -= 2;
+
+		else if (i >= 1)
+			i -= 1;
+		else
+			break;
+		change += 1;
+	}
+	printf("%d\n", change);
+	return (0);
+}
