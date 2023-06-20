636550eeb57231e1565686516af09774f7dd1d83
danimohlife <danimohlife@yahoo.com>
2023-06-20 17:35:24 +0100

    free memory

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * free_dog - free dog memory
+ * @d: pointer 
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
