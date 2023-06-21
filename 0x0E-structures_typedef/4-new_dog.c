4d35de9ceb456730184ae4c0ce54c3f5d8ecadbb
danimohlife <danimohlife@yahoo.com>
2023-06-21 05:23:43 +0100

    4 code rewrite

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,42 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
- * new_dog - struct funct
- * @name: name of dog
- * @age: dog age
- * @owner: dog owner
- * Return: pointer
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Pointer to the newly created dog struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
 
-	ptr = malloc(sizeof(dog_t));
-
-	if (ptr == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
-	ptr->name = name;
-	ptr->owner = owner;
-	ptr->age = age;
 
-	return (ptr);
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+		free(new_dog);
+		free(name_copy);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
+
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	return (new_dog);
 }
