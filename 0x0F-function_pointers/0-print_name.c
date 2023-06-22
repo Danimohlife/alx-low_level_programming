b24142a911d2adacbdea686a7007e3d21bfc80ce
danimohlife <danimohlife@yahoo.com>
2023-06-22 03:27:43 +0100

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints nameb functs
+ * @name: name to printed
+ * @f: void functs
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/REAME.md
@@ -0,0 +1,1 @@
+Just like normal variable we can also save the address of a function in C programming. But unlike normal pointer the point data function pointer point to code.
