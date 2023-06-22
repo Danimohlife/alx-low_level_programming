2e39bd24f60552ac3980c4e2ccd465a590fcb8cb
danimohlife <danimohlife@yahoo.com>
2023-06-22 04:04:48 +0100

    rewrite after failed to compile by cheacker

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,15 @@
-#include "function_pointers.h"
+#include "function_pointer.h"
 #include <stdlib.h>
 /**
- * print_name - prints nameb functs
- * @name: name to printed
- * @f: void functs
- * Return: nothing
+ * print_name - print pointer funct
+ * @name: pointer name
+ * @f: pointer to funct
+ * Return: nothin
  */
-
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *));
 {
-	f(name);
-}
-void print(char *name)
-{
-	while (*name != '\0')
+	if (name != NULL && f != NULL)
 	{
-		_putchar(*name);
-		name++;
 	}
-	_putchar('\n');
+	f(name);
 }
