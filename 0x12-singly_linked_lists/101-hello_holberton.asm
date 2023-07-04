fd4d9e4e1f09158a0a9a145939eea127acfc307b
danimohlife <danimohlife@yahoo.com>
2023-07-04 02:51:00 +0100

    assemble

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,24 @@
 section .data
-    hello_msg db "Hello, Holberton", 0
-    format db "%s", 10, 0
+	hello_msg db "Hello, Holberton", 0
+	format db "%s", 10, 0
 
 section .text
-    global main
+	global main
 
 extern printf
 
 main:
-    ; Set up the stack frame
-    push rbp
-    mov rbp, rsp
+	; Set up the stack frame
+	push rbp
+	mov rbp, rsp
 
-    ; Set up the arguments for printf
-    mov rdi, format ; Format string
-    mov rsi, hello_msg ; Message to print
+	; Set up the arguments for printf
+	mov rdi, format ; Format string
+	mov rsi, hello_msg ; Message to print
 
-    ; Call printf function
-    call printf
+	; Call printf function
+	call printf
 
-    ; Restore the stack frame and return
-    pop rbp
-    ret
+	; Restore the stack frame and return
+	pop rbp
+	ret
