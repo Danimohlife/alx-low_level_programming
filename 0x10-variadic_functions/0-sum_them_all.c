02e956d01a550a45d319382f187f29e052ba71a4
danimohlife <danimohlife@yahoo.com>
2023-07-05 11:32:10 +0100

    add the all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - varia func that add all argc
+ * @n: arg
+ * Return: total
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+	
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+This about vari.... function. function whereby you only declare with one paramenta
+But can call it with more than one arguments.
+
