191d3e89a109b6a5f8e3e36c4be2f6939fff0223
danimohlife <danimohlife@yahoo.com>
2023-07-05 12:19:10 +0100

    show the string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_string - func to print string
+ *  @separator: space
+ *  @n: const aurg
+ *  Return: string
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *str;
+	unsigned int i;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+		{
+			printf("%s", str);
+			if (str != NULL && n - i != 1)
+				printf(", ");
+		}
+        }
+
+        if (separator != NULL && i < n - 1)
+		printf("%s", separator);
+	
+	va_end(args);
+	printf("\n");
+}
