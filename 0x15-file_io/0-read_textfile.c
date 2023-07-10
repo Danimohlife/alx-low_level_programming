5dd7e71fc54b554e7f4929c13a34b8e9cb1adcf4
danimohlife <danimohlife@yahoo.com>
2023-07-10 21:58:46 +0100

    Feach and read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,11 +5,6 @@
  * @letters: num of lettr
  * Return: str / c
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
