43560b1f7d0ea1abca508575f3d3a10a515fecdc
ddanimoh <ddanimoh@gmail.com>
2023-07-31 14:53:06 +0100

    redo 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * _strcpy - string cpy
+ * _strcpy - string copy
  * @dest: destination file
  * @src: source file
- * Retrun: Destination file
+ * Return: Destination file
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+
+	if (*src == "")
+		return (dest);
 
 	while (src[i] != '\0')
 	{
