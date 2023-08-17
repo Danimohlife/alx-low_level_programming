a18f35156ec77e0cb5f223fc4624ee5ac56cc237
danimohlife <danimohlife@yahoo.com>
2023-08-17 20:49:45 +0100

    del at index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - DEleTE AT index N
+ * @head: To struct
+ * @index: index to del from
+ * Return: 1 / -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	current = *head;
+	for (unsigned int i = 0; current != NULL && i < index; i++)
+		current = current->next;
+
+	if (current == NULL)
+		return (-1);
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	else
+		*head = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
+}
