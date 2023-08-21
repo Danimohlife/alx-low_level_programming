082ee2f4224fedfc24ce266a42e56076a60b2a4b
danimohlife <danimohlife@yahoo.com>
2023-08-21 17:13:27 +0100

    .so

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 #include <stdlib.h>
 
-int _putchar(char c); int _islower(int c);
+int _putchar(char c);
+int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
